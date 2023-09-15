26de56d0f2b6708a08265d3ba3957c39d4d80384
electimon <electimon@gmail.com>
2023-09-15 19:45:40 +0000

    cypfq: set A/B configs

--- device.mk
+++ device.mk
@@ -4,5 +4,30 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# A/B support
+AB_OTA_UPDATER := true
+PRODUCT_SHIPPING_API_LEVEL := 30
+
+AB_OTA_PARTITIONS += \
+    boot \
+    dtbo \
+    product \
+    system \
+    system_ext \
+    vendor \
+    vendor_boot \
+    vbmeta \
+    vbmeta_system
+
+# A/B OTA dexopt update_engine hookup
+AB_OTA_POSTINSTALL_CONFIG ?= \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+# Dynamic Partitions
+TARGET_USES_DYNAMIC_PARTITIONS := true
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/motorola/cypfq/cypfq-vendor.mk)
