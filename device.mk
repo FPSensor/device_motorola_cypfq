07eab6009684ee182da1724f5c7ba13eedf41764
LuK1337 <priv.luk@gmail.com>
2023-09-16 04:31:18 +0000

    cypfq: build sensor packages

--- device.mk
+++ device.mk
@@ -250,6 +250,12 @@ PRODUCT_PACKAGES += \
     librmnetctl \
     libxml2
 
+# Sensors
+PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0.vendor \
+    android.hardware.sensors@2.0-service.multihal \
+    libsensorndkbridge
+
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common \
